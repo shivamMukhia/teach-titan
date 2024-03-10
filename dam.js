54183bb21dbd430a33fdcf5575e9688e9a055a09
Rohan Mandal <139258819+rohanmaan07@users.noreply.github.com>
2024-03-10 18:13:03 +0530

    dam.js

---
+++ dam.js
@@ -0,0 +1,121 @@
+const Dam = [
+  //Andhra pradesh.....
+    {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [79.31440664523755,16.58095461504516 ]
+        },
+        "properties": {
+          "name": "Nagarjuna Sagar Dam",
+          "address":  " Hydroelectric power generation, irrigation, and drinking water supply,  Nalgonda and Guntur districts"
+          
+         
+        }
+      },
+
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [78.90159295711634,16.0890073536662]
+        },
+        "properties": {
+          "name": "Srisailam Dam",
+          "address": "Hydroelectric power generation and irrigation, Krishna River,Kurnool district"
+          
+         
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [80.05090004499924,16.728689059817807]
+        },
+        "properties": {
+          "name": "Pulichinthala Project (Vijayawada) Dam",
+          "address": " Irrigation and drinking water supply, Krishna River, Krishna district"
+          
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [77.92357762178827,18.20257807028815]
+        },
+        "properties": {
+          "name": "Nizam Sagar Dam",
+          "address": " Irrigation and drinking water supply, Manjira River, Nizamabad district"
+        
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [ 76.33795581057498, 15.261074499953239]
+        },
+        "properties": {
+          "name": "Tungabhadra Dam",
+          "address": " Irrigation and drinking water supply, Tungabhadra River,Kurnool district"
+          
+        
+        }
+      },
+      //Assam...
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [ 94.2588131338426, 27.552649144256282]
+        },
+        "properties": {
+          "name": "Subansiri Lower Dam",
+          "address": " Hydroelectric power generation, Subansiri River,North Lakhimpur  district"
+          
+        
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [ 92.51725383101642, 25.955204662306347]
+        },
+        "properties": {
+          "name": "Karbi Langpi Dam",
+          "address": " Irrigation and flood control, Kopili River,Karbi Anglong district"
+          
+          
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [ 92.68273238318432, 27.16494693609338]
+        },
+        "properties": {
+          "name": "Kameng Dam",
+          "address": " Hydroelectric power generation, Kameng (also known as Jia Bharali) River, West Kameng  district"
+          
+          
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [ 92.63515151285756,25.529307485186795], 
+        },
+        "properties": {
+          "name": "Kopili Dam",
+          "address": "Hydroelectric power generation, Kopili River, Dima Hasao  district"
+          
+          
+        }
+      },
+    
+    ]
