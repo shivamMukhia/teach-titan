7249363849bed6eb2c5e96d131ba8ddff5426822
shivam <shivammukhia1@gmail.com>
2024-03-12 17:09:46 +0530

    frontend ready

---
+++ public/assam.js
@@ -0,0 +1,58 @@
+const myMapA = L.map('map').setView([24.732851541578558, 87.6742875145633], 6.6);
+const tileUrlA = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const attributionA ='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>';
+const tileLayerA = L.tileLayer(tileUrlA, { attributionA });
+tileLayerA.addTo(myMapA);
+
+function Popup(Dam) {
+    return `
+      <div class="leaflet-popup-content-wrapper dam-popup">
+            <h4>${Dam.properties.name}</h4>
+            <p>${Dam.properties.address}</p> 
+        </div>
+    `;
+  }
+  
+  function Popup(Plants) {
+    return `
+    <div class="leaflet-popup-content-wrapper plant-popup">
+    <h4>${Plants.properties.name}</h4>
+    <p>${Plants.properties.address}</p> 
+</div>
+    `;
+  }
+  
+  
+function Feature(feature, layer) {
+    layer.bindPopup(Popup(feature));
+}
+
+function PlantFeature(Plantfeature, layer) {
+    layer.bindPopup(Popup(Plantfeature));
+}
+
+// let myIcon = L.icon({
+//     iconUrl: 'png.png',
+//     iconSize: [30, 40]
+// });
+
+
+const LayerA = L.geoJSON(Dam, {
+    onEachFeature: Feature,
+    pointToLayer: function(feature, latlng) {
+        return L.marker(latlng);   //,{ icon: myIcon }
+    }
+});
+LayerA.addTo(myMapA);
+
+let myIconA= L.icon({
+    iconUrl: 'download.jpg',
+    iconSize: [30, 40]
+});
+const PlantLayerA = L.geoJSON(Plants, {
+    onEachFeature: PlantFeature,
+    pointToLayer: function(feature, latlng) {
+        return L.marker(latlng,{ icon: myIconA });   //,
+    }
+});
+PlantLayerA.addTo(myMapA);
---
+++ public/plant.js
@@ -0,0 +1,559 @@
+const Plants = [
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [93.91127260900566,24.823839477275456,]
+        },
+        "properties": {
+            "name": "Lamphelpat sewage treatment plant",
+            "address": "it is situated in RIMS doctors colony, it serves the water needs of the city and surrounding areas"
+        }
+    },
+    
+    //Meghalaya
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [92.37357384654324,25.17924556366872]
+        },
+        "properties": {
+            "name": "Lumshnong water treatment plant",
+            "address": "it is situated in Lumshnong, it serves the water needs of the city and surrounding areas"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [91.92642023918593,25.67720692324327]
+        },
+        "properties": {
+            "name": "Umiam water treatment plant",
+            "address": "it is situated in Umiam, it serves the water needs of the city and surrounding areas"
+        }
+    },
+    {
+        //Mizoram
+
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [92.7132468468281,23.730031882549593]
+        },
+        "properties": {
+            "name": "Aizawl water treatment plant",
+            "address": "it is situated in Aizawal district, it serves the water needs of the city and surrounding areas"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [93.1514681738342,22.3095517073324]
+        },
+        "properties": {
+            "name": "Saiha water treatment plant",
+            "address": "it is situated in nongmaiban, it serves the water needs of the city and surrounding areas"
+        }
+    },
+    {
+        //Nagaland
+
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [93.72624313738409,25.909007833906998]
+        },
+        "properties": {
+            "name": "Dimapur water treatment plant",
+            "address": "it is situated in Dimapur district, it serves the water needs of the city and surrounding areas"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [94.11023768739366,25.74793621429291]
+        },
+        "properties": {
+            "name": "Kohima water treatment plant",
+            "address": "it is situated in Kohima district, it serves the water needs of the city and surrounding areas"
+        }
+    },
+    {
+        //Odisha
+
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [85.9001145668899,20.465926534261406]
+        },
+        "properties": {
+            "name": "Cuttack water treatment plant",
+            "address": "it is situated in Cuttack district, it serves the water needs of the city and surrounding areas"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [85.84243634999899,20.266694989235614]
+        },
+        "properties": {
+            "name": "Bhubaneswar water treatment plant",
+            "address": "it is situated in Bhubaneswar, it serves the water needs of the city and surrounding areas"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [74.87228309457093,31.63248373554766]
+        },
+        "properties": {
+            "name": "Amritsar water treatment plant",
+            "address": "it is situated in Amritsar, it serves the water needs of the city and surrounding areas"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [75.84869290908104,30.897659050290823]
+        },
+        "properties": {
+            "name": "Ludhiana water treatment plant",
+            "address": "it is situated in Ludhiana, it serves the water needs of the city and surrounding areas"
+        }
+    },
+    //(1-7 states Plants)
+    //Andhra Pradesh
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [83.18485479122057,17.65733190313209]
+        },
+        "properties": {
+            "name": "Visakhapatnam Water Treatment Plant",
+            "address": "Located in Visakhapatnam, it serves the water needs of the city and surrounding areas"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [79.40631812792746,13.648175712406045]
+        },
+        "properties": {
+            "name": "Tirupati Water Treatment Plant",
+            "address": "Serves the water needs of Tirupati city and surrounding areas"
+        }
+    },
+    //Assam
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [91.75082289587577,26.17116516311798]
+        },
+        "properties": {
+            "name": "Guwahati Water Supply Project",
+            "address": "Provide treated water to Guwahati city and surrounding areas"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [94.92406392182185,27.50040071287909]
+        },
+        "properties": {
+            "name": "Dibrugarh Water Treatment Plant",
+            "address": "Provide treated water to Dibrugarh city and nearby regions"
+        }
+    },
+    //Bihar
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [85.1406394112388,25.600696697401183]
+        },
+        "properties": {
+            "name": "Patna Water Supply Project",
+            "address": "Provide treated water to Patna city and surroundi"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [85.39003567490661,26.120503334382356]
+        },
+        "properties": {
+            "name": "Muzaffarpur Water Treatment Plant",
+            "address": "Provide treated water to Muzaffarpur city and nearby regions"
+        }
+    },
+    // Chhattisgarh
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [81.62836024168828,21.252343396970808]
+        },
+        "properties": {
+            "name": "Raipur Water Supply Project",
+            "address": "Provide treated water to Raipur city and surrounding areas"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [82.16943684775998,22.05644979099953]
+        },
+        "properties": {
+            "name": "Bilaspur Water Treatment Plant",
+            "address": "Provide treated water to Bilaspur city and nearby regions"
+        }
+    },
+    // Goa
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [73.89848971030887,15.619577887755062]
+        },
+        "properties": {
+            "name": "Assonora Water Treatment Plant",
+            "address": "Provide treated water to Bardez, Bicholim, and Sattari Talukas"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [73.81420398264986,15.54252336492672]
+        },
+        "properties": {
+            "name": "Porvorim Water Treatment Plant",
+            "address": " Provide treated water to Bardez, Tiswadi, and Ponda Talukas"
+        }
+    },
+    // Gujrat
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [72.8308639208353,21.168388753053943]
+        },
+        "properties": {
+            "name": "Surat Water Treatment Plant",
+            "address": "Provide treated water to Surat city and nearby regions"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [72.56169890867781,23.031067223364705]
+        },
+        "properties": {
+            "name": "Ahmedabad Water Treatment Plant",
+            "address": " Provide treated water to Ahmedabad city and surrounding"
+        }
+    },
+    // (21-28 states plant)
+    {//rajasthan
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [75.79708994582323,26.9176334329506]
+        },
+        "properties": {
+            "name": "Jaipur Water Treatment Plant",
+            "address": " Provide treated water to Jaipur city and nearby regions"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [73.02304237154698,26.23969347054411]
+        },
+        "properties": {
+            "name": "Jodhpur Water Treatment Plant",
+            "address": "Provide treated water to Jodhpur city and nearby regions"
+        }
+    },
+    // sikkim
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [88.61169707967137,27.330700503851794]
+        },
+        "properties": {
+            "name": "Gangtok Water Treatment Plant",
+            "address": " Provide treated water to Gangtok city and nearby areas"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [88.3926571845738,27.136552285274707]
+        },
+        "properties": {
+            "name": "Singtam Water Treatment Plant",
+            "address": "Provide treated water to Singtam town and surrounding regions"
+        }
+    },
+    // Tamil Nadu
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [80.27981770740566,13.090449653883082]
+        },
+        "properties": {
+            "name": "Chennai Metro Water Treatment Plants",
+            "address": "Coimbatore Water Supply and Sewerage Treatment Plants"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [76.96686794949338,11.017582347701618]
+        },
+        "properties": {
+            "name": "Coimbatore Water Supply and Sewerage Treatment Plants",
+            "address": "Provide treated water and manage sewerage for Coimbatore city and nearby regions"
+        }
+    },
+    //telangana
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [78.47622217149866,17.406715858208536]
+        },
+        "properties": {
+            "name": "Hyderabad Metropolitan Water Supply and Sewerage Board (HMWSSB) Treatment Plants",
+            "address": "Provide treated water to Hyderabad city and neighboring regions"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [79.59545423735766,17.96539274188953]
+        },
+        "properties": {
+            "name": "Warangal Municipal Corporation Water Treatment Plant",
+            "address": "Provide treated water to Warangal city "
+        }
+    },
+    //Tripura
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [91.30277820682218,23.819631416628795]
+        },
+        "properties": {
+            "name": "Agartala Water Treatment Plant",
+            "address": "Provide treated water to Agartala city and surrounding areas"
+        }
+    },
+    // uttar Pradesh
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [80.33353594255719,26.442532206394027]
+        },
+        "properties": {
+            "name": "Ganga Jal Water Treatment Plant, Kanpur",
+            "address": " Provide treated water to Kanpur city "
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [78.00168803111113,27.168152495891285]
+        },
+        "properties": {
+            "name": "Agra Water Treatment Plant",
+            "address": "Provide treated water to Agra city"
+        }
+    },
+    //Uttarakhand
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [78.02936650654755,30.315510821489163]
+        },
+        "properties": {
+            "name": "Dehradun Water Treatment Plant",
+            "address": " Provide treated water to Dehradun city"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [78.16600895292459,29.94553163028824]
+        },
+        "properties": {
+            "name": "Haridwar Water Treatment Plant",
+            "address": "Provide treated water to Haridwar city and surrounding regions"
+        }
+    },
+    // West Bengal
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [88.3415147928592,22.590305746879547]
+        },
+        "properties": {
+            "name": "Kolkata Water Supply Project",
+            "address": "Provide treated water to Kolkata city and nearby areas"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [88.3415147928592,26.77173186383966]
+        },
+        "properties": {
+            "name": "Siliguri Water Treatment Plant",
+            "address": "Provide treated water to Siliguri city "
+        }
+    },
+    //(8-14 states plants)
+    // karnataka
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [77.59041270846173,12.975644247885267]
+        },
+        "properties": {
+            "name": "Bengaluru Water Treatment Plant",
+            "address": "Provide treated water to Bengaluru city and surrounding areas"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [76.64421529327542,12.29625368680935]
+        },
+        "properties": {
+            "name": "Mysuru Water Treatment Plant",
+            "address": "Provide treated water to Mysuru city and nearby regions"
+        }
+    },
+    //Kerala
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [76.68172358662795,9.325707666623819]
+        },
+        "properties": {
+            "name": "Aranmula Water Treatment Plant",
+            "address": "Provide treated water to Aranmula and surrounding areas"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [76.26698974136487,9.926860445574015]
+        },
+        "properties": {
+            "name": "Kochi Water Treatment Plant",
+            "address": "Provide treated water to Kochi city and nearby regions"
+        }
+    },
+    // Himachal Pradesh
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [77.16768191110084,31.111693070729533]
+        },
+        "properties": {
+            "name": "Shimla Water Treatment Plant",
+            "address": "Provide treated water to Shimla city and surrounding areas"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [77.11549685772336,31.956776387090674]
+        },
+        "properties": {
+            "name": "Kullu Water Treatment Plant",
+            "address": "Provide treated water to Kullu district and nearby regions"
+        }
+    },
+    // Madhya pradesh
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [77.41448911159587,23.257901296116536]
+        },
+        "properties": {
+            "name": "Bhopal Water Treatment Plant",
+            "address": " Provide treated water to Bhopal city and surrounding areas"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [75.85443067378505,22.71680956357097]
+        },
+        "properties": {
+            "name": "Indore Water Treatment Plant",
+            "address": "Provide treated water to Indore city and nearby regions"
+        }
+    },
+    // maharashtra
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [72.87791532303928,19.0757651666334]
+        },
+        "properties": {
+            "name": "Mumbai Water Treatment Plant",
+            "address": "Provide treated water to Mumbai city and surrounding areas"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [73.85295184667102,18.527162011829592]
+        },
+        "properties": {
+            "name": "Pune Water Treatment Plant",
+            "address": "Provide treated water to Pune city and nearby regions"
+        }
+    },
+]
